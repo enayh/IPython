99cec738e391c2ee63f1568527a0263d6444c582
enayh <n_perez@alumnos.upla.cl>
2022-05-25 14:57:44 -0400

    Update

---
+++ 11.- Bucles.py
@@ -0,0 +1,14 @@
+# Bucles
+
+# While
+contador = 0
+while(contador <= 10):
+    print("2 elevado a "+str(contador)+" es igual a: "+str(2**contador))
+    contador+=1
+
+print("####################################################")
+
+# For
+
+for contador in range(11):
+    print("2 elevado a "+str(contador)+" es igual a: "+str(2**contador))
