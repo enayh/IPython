59eae404b23f3271d3519d17b1606a7366a4c271
enayh <n_perez@alumnos.upla.cl>
2022-05-25 16:52:27 -0400

    Update

---
+++ 15.- Listas.py
@@ -0,0 +1,5 @@
+# Listas
+
+lista = [1, 2, 3, 4, 5, 'Nicolas']
+print(lista)
+print(len(lista))
---
+++ 16.- Tuplas.py
@@ -0,0 +1,7 @@
+# Tuplas (INMUTABLES)
+
+tupla = (1, 2 , 3, 4, 5, 6)
+print(tupla)
+
+dias_semana = ('Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab', 'Dom')
+print(dias_semana)
