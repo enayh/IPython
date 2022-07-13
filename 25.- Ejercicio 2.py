2e135a7866f9f007f5954309f5d1547cf6ff311e
Nicolas <n_perez@alumnos.upla.cl>
2022-07-13 19:52:56 -0400

    :)

--- 25.- Ejercicio 2.py
+++ 25.- Ejercicio 2.py
@@ -1,1 +1,11 @@
 # Escribir un programa que lea un archivo del "Ejercicio 1.py" y agregue cada linea a una lista y luego imprima la lista.
+
+def main():
+    lista = []
+    with open('./archivos/tabla-4.txt', 'r') as file:
+        for linea in file:
+            lista.append(linea)
+    print(lista)
+
+if __name__ == '__main__':
+    main()
